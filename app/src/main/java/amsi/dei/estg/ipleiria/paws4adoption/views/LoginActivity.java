58da86c35248d7c0550814cef08da40df219cff0
Simão Pedro <2192579@my.ipleiria.pt>
2020-12-21 22:44:45 +0000

    PROJ-5 Added btn Sirgup on Login view

--- app/src/main/java/amsi/dei/estg/ipleiria/paws4adoption/views/LoginActivity.java
+++ app/src/main/java/amsi/dei/estg/ipleiria/paws4adoption/views/LoginActivity.java
@@ -6,6 +6,7 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -20,6 +21,7 @@ public class LoginActivity extends AppCompatActivity implements LoginListener{
 
     private EditText etUsername;
     private EditText etPassword;
+    private Button btnSignup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +30,7 @@ public class LoginActivity extends AppCompatActivity implements LoginListener{
 
         etUsername = findViewById(R.id.etUsername);
         etPassword = findViewById(R.id.etPassword);
+        btnSignup = findViewById(R.id.btnSignup);
 
         SingletonPawsManager.getInstance(getApplicationContext()).setLoginListener(this);
     }
@@ -95,4 +98,8 @@ public class LoginActivity extends AppCompatActivity implements LoginListener{
         }
     }
 
+    public void onClickSignup(View view) {
+        Intent intent = new Intent(getApplicationContext(), SignupActivity.class);
+        startActivity(intent);
+    }
 }
